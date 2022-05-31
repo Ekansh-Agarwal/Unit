68cedd047bad9aa04945d93eed413500f634ffaa
Ekansh-Agarwal <77278829+Ekansh-Agarwal@users.noreply.github.com>
2022-05-31 08:41:19 +0530

    function added

--- MainActivity.java
+++ MainActivity.java
@@ -9,7 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+//onCreate function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
